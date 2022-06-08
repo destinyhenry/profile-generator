72d615c038df49eb2a8152e4221b71bacdf3eb29
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-06-07 23:50:25 -0400

    delete other files

--- employees.test.js
+++
@@ -1,8 +0,0 @@
-const Employee = require('')
-const employee = new Employee('Sasha', '070996', 'sashacodes@codes.com');
-
-test ('testing employee constructor', () => {
-    expect(employee.name).toBe('Sasha');
-    expect(employee.id).toBe('070996');
-    expect(employee.email).toBe('sashacodes@codes.com');
-});
